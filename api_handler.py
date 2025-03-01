1c90b311fc575d48ba96bb48fb3ad8acd53c04a4
Cristian Marius <cristianmariusv97@gmail.com>
2025-03-01 15:31:57 +0200

    Saved file in uploads folder and save metadata in db. Added todo for future preliminary check

--- api_handler.py
+++ api_handler.py
@@ -14,7 +14,8 @@ api_blueprint = Blueprint('api', __name__)
 @api_blueprint.route('/upload', methods=['POST'])
 def upload_csv():
     """
-    Expect file as .csv in body of the request.
+    API endpoint: /upload
+    Expect file as .csv in body of the request. Check if file is .csv format
     Example cURL:
         curl    --location 'http://127.0.0.1:5000/upload' \
                 --form 'file=@"/stock_prices.csv"'
@@ -22,17 +23,34 @@ def upload_csv():
     """
     if 'file' not in request.files:
         logger.error("No file part in request")
-        return jsonify({"error": "No file part"}), 400
+        return jsonify({"error": "No file part in request body."}), 400
 
     file = request.files['file']
 
     if file.filename == '':
         logger.error("No selected file")
-        return jsonify({"error": "No selected file"}), 400
+        return jsonify({"error": "No selected file."}), 400
 
     if not file.filename.endswith('.csv'):
         logger.error("Invalid file type")
-        return jsonify({"error": "Invalid file type. Only CSV allowed"}), 400
+        return jsonify({"error": "Invalid file type. Only CSV allowed."}), 400
+
+    """
+    TO ADD
+    Preliminary check of the csv file. Make sure there are no nulls. 
+    If nulls are found solve them depending on type of data in the column.
+    Ex. If null in datetime column drop row or calculate timedelta between points and use it to determine missing point.
+    If null in value column, make it 0.
+    If null in object (str) type column fill in from previous row.
+    If multiple values in str type column notify the user about it and keep only rows with most appearances. 
+    """
+
+    # Save file in UPLOADS folder.
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(file_path)
+
+    # Save metadata in DB.
+    save_csv_metadata(file.filename, file_path)
 
     logger.info(f"File {file.filename} uploaded successfully")
     return jsonify({"message": "File uploaded successfully", "filename": file.filename}), 200
