8a3863dca7a318442eda2f6a33b63915401b7ebd
Cristian Marius <cristianmariusv97@gmail.com>
2025-03-01 15:20:15 +0200

    Created /upload endpoint which checks file validity

--- api_handler.py
+++ api_handler.py
@@ -1,13 +1,41 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+import pandas as pd
+import os
+import random
+from db_handler import save_csv_metadata
+from logging_handler import logger
+from config import UPLOAD_FOLDER
 
 
 # Create API Blueprint for modularity.
 api_blueprint = Blueprint('api', __name__)
 
 
-@api_blueprint.route('/test', methods=['GET'])
-def hello_world():
-    return "Hello", 200
+@api_blueprint.route('/upload', methods=['POST'])
+def upload_csv():
+    """
+    Expect file as .csv in body of the request.
+    Example cURL:
+        curl    --location 'http://127.0.0.1:5000/upload' \
+                --form 'file=@"/stock_prices.csv"'
+    :return: Response
+    """
+    if 'file' not in request.files:
+        logger.error("No file part in request")
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        logger.error("No selected file")
+        return jsonify({"error": "No selected file"}), 400
+
+    if not file.filename.endswith('.csv'):
+        logger.error("Invalid file type")
+        return jsonify({"error": "Invalid file type. Only CSV allowed"}), 400
+
+    logger.info(f"File {file.filename} uploaded successfully")
+    return jsonify({"message": "File uploaded successfully", "filename": file.filename}), 200
 
 
 if __name__ == '__main__':
