813c5c633ff024917162a1273cbbc40242090d9f
Cristian Marius <cristianmariusv97@gmail.com>
2025-03-01 14:58:12 +0200

    Created config.py for folder structures, paths,etc

---
+++ config.py
@@ -0,0 +1,11 @@
+# FOLDER STRUCTURES, PATHS, FILES
+
+UPLOAD_FOLDER = "uploads"
+
+DB_FOLDER = "database"
+DB_FILE = "data.db"
+DB_PATH = f"{DB_FOLDER}/{DB_FILE}"
+
+LOG_FOLDER = "logs"
+LOG_FILE = "app.log"
+LOG_PATH = f"{LOG_FOLDER}/{LOG_FILE}"
